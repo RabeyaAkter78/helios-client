ecc50d05d52f35bf7ce8c6f40e5752136ad8c33b
RabeyaAkter78 <rabeyariya251@gmail.com>
2024-01-28 22:20:10 +0600

    added ,eft side home bar

--- src/Components/Pages/Home/Home/Home.jsx
+++ src/Components/Pages/Home/Home/Home.jsx
@@ -1,10 +1,45 @@
+import LeftSideHome from "../LeftSideHome/LeftSideHome";
 
 const Home = () => {
-    return (
-        <div className='bg-gray-100'>
-            <h1>this is home</h1>
+//   const [posts, setPosts] = useState([]);
+//   useEffect(() => {
+//     fetch(`${import.meta.env.VITE_URL}/allposts`)
+//       .then((res) => res.json())
+//       .then((data) => {
+//         setPosts(data);
+//       });
+//   }, []);
+
+  return (
+    <>
+      <div>
+        <div className="grid grid-cols-1 md:grid-cols-12">
+          <div className="lg:col-span-3 hidden lg:block ">
+            <LeftSideHome />
+          </div>
+
+          <div className="grid-cols-12 md:col-span-6">
+      <p>lorem200</p>
+            {/* <div >
+              <CreateStorySlider/>
+            </div>
+            <div>
+              <Post></Post>
+            </div>
+            <div className=" grid  gap-3 ">
+              {posts?.map((post) => (
+                <DisplayPost key={post?._id} post={post} />
+              ))}
+            </div> */}
+          </div>
+
+          {/* <div className=" lg:col-span-3 hidden lg:block text-black  ">
+            <RightSideBar />
+          </div> */}
         </div>
-    );
+      </div>
+    </>
+  );
 };
 
 export default Home;
---
+++ src/Components/Pages/Home/LeftSideHome/LeftSideHome.jsx
@@ -0,0 +1,168 @@
+import { MdOutlineWatchLater } from "react-icons/md";
+import { CiDeliveryTruck } from "react-icons/ci";
+
+
+
+const LeftSideHome = () => {
+
+  return (
+    <div className="p-5 mb-5">
+
+      <div className="flex flex-col justify-start items-start   p-2 gap-2">
+        <h3 className="text-center text-2xl font-semibold "
+        >
+          Доставка
+        </h3>
+        <div className="flex justify-start items-start  gap-2">
+          <MdOutlineWatchLater />
+          <p>10 – 30 min</p>
+        </div>
+        <div className="flex justify-start items-start  gap-2 ">
+          <CiDeliveryTruck />
+          <p>на первый заказ бесплатно от
+            1  000 ₸</p>
+        </div>
+      </div>
+
+      <div className="flex flex-col justify-start items-start  p-2 gap-2">
+        <h3 className="text-center text-2xl font-semibold "
+        >
+          Catalog
+        </h3>
+        <button className="btn hover:btn-ghost flex justify-start items-start  gap-2">
+          <MdOutlineWatchLater />
+          <p>Холодные напитки</p>
+        </button>
+        <button className="btn hover:btn-ghost flex justify-start items-start  gap-2">
+          <MdOutlineWatchLater />
+          <p>Выпечка</p>
+        </button>
+        <button className="btn hover:btn-ghost flex justify-start items-start  gap-2">
+          <MdOutlineWatchLater />
+          <p>Хот-доги</p>
+        </button>
+        <button className="btn hover:btn-ghost flex justify-start items-start  gap-2">
+          <MdOutlineWatchLater />
+          <p>
+            Красота и гигиена</p>
+        </button>
+        <button className="btn hover:btn-ghost flex justify-start items-start  gap-2">
+          <MdOutlineWatchLater />
+          <p>Пицца</p>
+        </button>
+        <button className="btn hover:btn-ghost flex justify-start items-start  gap-2">
+          <MdOutlineWatchLater />
+          <p>Горячие напитки</p>
+        </button>
+        <button className="btn hover:btn-ghost flex justify-start items-start  gap-2">
+          <MdOutlineWatchLater />
+          <p>Бакалея</p>
+        </button>
+        <button className="btn hover:btn-ghost flex justify-start items-start  gap-2">
+          <MdOutlineWatchLater />
+          <p>Семечки, чипсы</p>
+        </button>
+        <button className="btn hover:btn-ghost flex justify-start items-start  gap-2">
+          <MdOutlineWatchLater />
+          <p>Товары для быта</p>
+        </button>
+        <button className="btn hover:btn-ghost flex justify-start items-start  gap-2">
+          <MdOutlineWatchLater />
+          <p>Семечки, чипсы</p>
+        </button>
+        <button className="btn hover:btn-ghost flex justify-start items-start  gap-2">
+          <MdOutlineWatchLater />
+          <p>Сэндвичи</p>
+        </button>
+        <button className="btn hover:btn-ghost flex justify-start items-start  gap-2">
+          <MdOutlineWatchLater />
+          <p>Готовая еда</p>
+        </button>
+        <button className="btn hover:btn-ghost flex justify-start items-start  gap-2">
+          <MdOutlineWatchLater />
+          <p>Сэндвичи</p>
+        </button>
+        <button className="btn hover:btn-ghost flex justify-start items-start  gap-2">
+          <MdOutlineWatchLater />
+          <p>Десерты</p>
+        </button>
+        <button className="btn hover:btn-ghost flex justify-start items-start  gap-2">
+          <MdOutlineWatchLater />
+          <p>Для детей</p>
+        </button>
+        <button className="btn hover:btn-ghost flex justify-start items-start  gap-2">
+          <MdOutlineWatchLater />
+          <p>
+            Жевательные резинки</p>
+        </button>
+        <button className="btn hover:btn-ghost flex justify-start items-start  gap-2">
+          <MdOutlineWatchLater />
+          <p>
+            Кондитерские изделия</p>
+        </button>
+        <h3 className="text-center text-2xl font-semibold "
+        >
+          Catalog
+        </h3>
+        <button className="btn hover:btn-ghost flex justify-start items-start  gap-2">
+          <MdOutlineWatchLater />
+          <p>
+            Молочные продукты</p>
+        </button>
+        <button className="btn hover:btn-ghost flex justify-start items-start  gap-2">
+          <MdOutlineWatchLater />
+          <p>
+            Колбасы, сыры</p>
+        </button>
+        <button className="btn hover:btn-ghost flex justify-start items-start  gap-2">
+          <MdOutlineWatchLater />
+          <p>
+            Крупы, макароны</p>
+        </button>
+        <button className="btn hover:btn-ghost flex justify-start items-start  gap-2">
+          <MdOutlineWatchLater />
+          <p>
+            Орехи и сухофрукты</p>
+        </button>
+        <button className="btn hover:btn-ghost flex justify-start items-start  gap-2">
+          <MdOutlineWatchLater />
+          <p>
+            Для животных</p>
+        </button>
+        <button className="btn hover:btn-ghost flex justify-start items-start  gap-2">
+          <MdOutlineWatchLater />
+          <p>
+            Медицинские товары</p>
+        </button>
+        <button className="btn hover:btn-ghost flex justify-start items-start  gap-2">
+          <MdOutlineWatchLater />
+          <p>
+
+            Автопринадлежности</p>
+        </button>
+        <button className="btn hover:btn-ghost flex justify-start items-start  gap-2">
+          <MdOutlineWatchLater />
+          <p>
+
+            Игрушки</p>
+        </button>
+        <button className="btn hover:btn-ghost flex justify-start items-start  gap-2">
+          <MdOutlineWatchLater />
+          <p>
+
+            Зарядные устройства</p>
+        </button>
+        <button className="btn hover:btn-ghost flex justify-start items-start  gap-2">
+          <MdOutlineWatchLater />
+          <p>
+
+            Дом, дача, отдых</p>
+        </button>
+
+      </div>
+    </div>
+
+  );
+};
+
+export default LeftSideHome;
